d2c0cabd3575e257bfe1f8bc415f0233c51a42d5
Ashitom <sasukeuchia32828@gmail.com>
2025-05-31 16:02:22 +0545

    chore: update middleware to protect project route instead of dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,39 +6,39 @@ export async function middleware(req: NextRequest) {
   console.log("Middleware triggered for request:", req.url);
 
   // admin route check
-  const isAdminRoute =req.url.startsWith('/admin')  ||  req.url.includes('/admin');
-    if (isAdminRoute ) {
+  const isAdminRoute =
+    req.url.startsWith("/admin") || req.url.includes("/admin");
+  if (isAdminRoute) {
     console.log("ADMIN ROUTE HIT", req.url);
     const token = await getToken({ req });
     console.log("ADMIN TOKEN", token);
     if (!token) {
       // Redirect to admin login page if no token
-      return NextResponse.redirect(new URL('/adminauth', req.url));
+      return NextResponse.redirect(new URL("/adminauth", req.url));
     }
 
-    
     // Admin role check
-    if (token.role !== 'admin') {
-      return NextResponse.redirect(new URL('/', req.url)); // Redirect to home if not an admin
+    if (token.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url)); // Redirect to home if not an admin
     }
   }
-  
-  // user route check
-  const isUserRoute =  req.url.includes('/settings') || req.url.includes('/dashboard');
 
-  if(isUserRoute)
-  {
+  // user route check
+  const isUserRoute =
+    req.url.includes("/settings") || req.url.includes("/projects");
+
+  if (isUserRoute) {
     console.log("USER ROUTE HIT", req.url);
     const token = await getToken({ req });
 
-    if(!token) {
+    if (!token) {
       // Redirect to user login page if no token
-      return NextResponse.redirect(new URL('/login', req.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
 
     // yo flow chai paxi design garnu parxa
-    if (token.role !== 'user') {
-      return NextResponse.redirect(new URL('/', req.url)); 
+    if (token.role !== "user") {
+      return NextResponse.redirect(new URL("/", req.url));
     }
   }
 
@@ -58,13 +58,9 @@ export const config = {
     "/api/auth/protected/:path*",
     "/settings",
     "/settings/:path*",
-    "/dashboard",
-    "/dashboard/:path*",
+    "/projects",
+    "/projects/:path*",
     "/admin",
     "/admin/:path*",
-    
   ],
 };
-
-
-
